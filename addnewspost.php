6ebcb080a9029abc3f30d03aedf3ba3479326a62
MULI <mmuullii2014@hotmail.com>
2020-08-08 02:08:11 +0100

    Added the Admin Newspost Design

--- addnewspost.php
+++ addnewspost.php
@@ -25,23 +25,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+    <link rel="stylesheet" href="css/style-add-news-post.css">
+    <title>Admin | Add News Post</title>
 </head>
 <body>
     <?php include "includes/Navigation-bar.php"; ?>
-    <form method="POST">
-        <input type="text" name="newsPostTitle" placeholder="Put your title here">
-        <textarea name="newsPostContent" id="" cols="30" rows="10"></textarea>
-        <p>Categories</p>
-        <?php 
-            $categories = $query->selectAll('category');
-            foreach($categories as $category){
-                echo '<input type="checkbox" name="selected[]" value="' . $category['id'] . '"> ' . $category['name'];
-                echo '<br>';
-            }
-        ?>
-        <input type="submit" name="submitNewsPost">
-    </form>
+    <div class="title1">
+        <h1 style="text-align: center; margin: 10px 0;">Add News Post</h1>
+        <hr>
+    </div>
+    <div class="add-news-post-main-container go-column">
+        <form class="go-column" method="POST">
+            <label for="newsPostTitle">Add your News Post HERE</label>
+            <input type="text" name="newsPostTitle" placeholder="Put your title here">
+            <textarea name="newsPostContent" id="" cols="30" rows="10" placeholder="Write something.. :D"></textarea>
+            <label for="selected[]">Categories</label>
+            <div class="categories-box go-row align-center">
+                <?php 
+                    $categories = $query->selectAll('category');
+                    foreach($categories as $category){
+                        
+                        echo '<input type="checkbox" name="selected[]" value="' . $category['id'] . '"> ' . $category['name'];  
+                        echo ' | ';              
+                    }
+                ?>
+            </div>
+
+            <div class="btn-submit justify-center">
+                <input type="submit" name="submitNewsPost">
+            </div>
+        </form>
+    </div>
     <?php include "includes/footer.php"; ?>
 </body>
 </html>
--- newspost.php
+++ newspost.php
@@ -29,14 +29,68 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+    <link rel="stylesheet" href="css/news-post.css">
+    <title>News Post</title>
 </head>
 <body>
     <?php include "includes/Navigation-bar.php"; ?>
-      
+    <div class="justify-center">
+        <h1 class="title1">News Post</h1>
+    </div>
+    <div class="example-container">
+        <div class="top go-row align-center">
+            <img src="profilepicture\default.jpg" alt="Admin-Profile-Pic">
+            <p id="admin-name">Flamur Fazliu</p> <i class="fas fa-angle-right"></i>
+            <p id='admin-category-indicator'>News/Categories/HTML</p>    
+            <div class="admin-dropdown-post-menu-container">
+                <button class="admin-dropdown-post-menu-button">···</button>
+                <ul>
+                    <li class="button-edit admin-list-button"><a href="#">Edit</a></li>
+                    <li class="button-remove admin-list-button"><a href="#">Remove</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="middle">
+            <hr class="admin-hr1">
+            <p id="admin-content-post">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem impedit qui cumque voluptas id, non accusantium commodi perferendis est deserunt rem eos aspernatur voluptates dolores ab neque facere, dolorum temporibus! Similique distinctio iste reprehenderit enim repellendus vitae necessitatibus id, provident a fuga quis eum illo architecto hic voluptate minima quod!</p>
+        </div>
 
+        <hr class="admin-hr2">
+        <div class="bottom justify-center">    
+            <a class="admin-button" href=""><i class="far fa-thumbs-up"></i></a>
+            <a class="admin-button" href=""><i class="far fa-comment-alt"></i></a>
+        </div>
+        <hr class="admin-hr2">
 
+        <div class="category-links">
+    
+            <a href="">HTML</a>
+            <a href="">CSS</a>
+            <a href="">Java Script</a>
+            <a href="">JAVA</a>
+            <a href="">PHP</a>
+            <a href="">SQL</a>
+        </div>
+
+        <div class="comments-main-wrapper">
+        <hr class="admin-hr1">
+            <div class="comments-wrapper go-row align-center">
+                <img src="profilepicture\default.jpg" alt="Admin-coment-Profile-Pic">
+                <div class="comment-content">
+                    <p id="comenter-name">Ardit Islami</p> 
+                    <p id='comenter-content'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt natus, nihil reprehenderit expedita suscipit nemo vero incidunt accusantium cum error explicabo, provident aperiam neque ex dolor voluptatibus doloremque, sit necessitatibus.</p> 
+                </div>   
+                <div class="dropdown-comment-menu-container">
+                    <button class="dropdown-comment-menu-button">···</button>
+                    <ul>
+                        <li class="button-edit commenter-list-button"><a href="#">Edit</a></li>
+                        <li class="button-remove commenter-list-button"><a href="#">Remove</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
     <?php include "includes/footer.php"; ?>
 </body>
 </html>
